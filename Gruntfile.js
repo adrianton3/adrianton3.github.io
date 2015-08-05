cc5ddd361ba9e54122a5b631836c0287b06ec312
Adrian Toncean <adrianton3@gmail.com>
2015-08-05 22:43:32 +0200

    Switched to using docco; first article draft

---
+++ Gruntfile.js
@@ -0,0 +1,83 @@
+module.exports = function (grunt) {
+	'use strict';
+
+	var fs = require('fs');
+
+	var _ = require('underscore');
+
+	var OUT_DIR = 'blog';
+	var TEMPLATE_DIR = 'tools/template';
+
+	function getDirs(basePath) {
+		return fs.readdirSync(basePath).filter(function (file) {
+			var completeFile = basePath + '/' + file;
+			return fs.statSync(completeFile).isDirectory();
+		});
+	}
+
+	function generateDoccoConfig() {
+		var dirs = getDirs('src');
+
+		var config = {};
+
+		dirs.forEach(function (dir) {
+			config[dir] = {
+				src: ['src/' + dir + '/*.js'],
+				options: {
+					output: OUT_DIR + '/art/' + dir,
+					template: TEMPLATE_DIR + '/docco.jst'
+				}
+			};
+		});
+
+		return config;
+	}
+
+	function readFile(path) {
+		return fs.readFileSync(path, 'utf8');
+	}
+
+	var headerTemplate = _.template(readFile(TEMPLATE_DIR + '/header.jst'));
+	var footerTemplate = _.template(readFile(TEMPLATE_DIR + '/footer.jst'));
+
+	function getHeader(path) {
+		var match = path.match(/([-\w]+)\/([-\w]+)\.html$/);
+		var title = match[1] + '-' + match[2];
+
+		return headerTemplate({
+			title: title,
+			css: '../../static/docco-small-tab.css'
+		});
+	}
+
+	function getFooter() {
+		return footerTemplate({});
+	}
+
+	grunt.initConfig({
+		docco: generateDoccoConfig(),
+		wrap: {
+			all: {
+				src: [OUT_DIR + '/art/**/*.html'],
+				dest: '',
+				options: {
+					wrapper: function (path) {
+						return [getHeader(path), getFooter()];
+					}
+				}
+			}
+		},
+		eslint: {
+			options: {
+				configFile: '.eslintrc'
+			},
+			target: ['Gruntfile.js', 'src/**/*.js', 'test/spec/**/*.js']
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-docco');
+	grunt.loadNpmTasks('grunt-eslint');
+	grunt.loadNpmTasks('grunt-wrap');
+
+	grunt.registerTask('default', ['docco', 'wrap']);
+};
---
+++ src/math-lib/math-lib.js
@@ -0,0 +1,96 @@
+// functional math lib
+// ===================
+
+// This is a short experiment showing the usage and implementation of a simple math lib.
+// The library should cover dealing with vectors and matrices.
+// This small snippet only covers vector types.
+
+// Let's first make a proposal for how the API should look like.
+var demo = function (implementation) {
+	'use strict';
+
+	var { make, getX, getY, getZ, add, scale, length } = implementation;
+
+	// Let's create 2 vectors
+	var v1 = make(1, 2, 3);
+	var v2 = make(4, 5, 6);
+
+	// And add them together
+	var sum = v1(add(v2));
+	// In coffeescript this can be rewritten without the parentheses like so
+	// `sum = v1 add v2`
+	// which is the most natural way of expressing this in a language without operator overloading
+
+	var doubleSum = sum(scale(2));
+	// Again, in coffeescript this looks way better `doubleSum = sum scale 2`<br>
+	// or, in expanded form `doubleSum = (v1 add v2) scale 2`
+
+	// So far so good, but how do we get the data back from the vector now?
+	var x = doubleSum(getX);
+};
+
+
+// Let's have a look at the implementation now.
+// --------------------------------------------
+var implementation = (function () {
+	'use strict';
+
+	// The "constructor" doesn't make use of any objects,
+	// `this` (and `new`), hidden state or TypedArrays.
+	// Everything is bound to a value and cannot be altered.
+	function make(x, y, z) {
+		return function vec3(fun) {
+			return fun(x, y, z);
+		}
+	}
+
+	// Accessors are probably the simplest function
+	// that can be applied on a vector
+	function getX(x, y, z) {
+		return x;
+	}
+
+	function getY(x, y, z) {
+		return y;
+	}
+
+	function getZ(x, y, z) {
+		return z;
+	}
+
+	// Operations, the minimum required
+	function add(that) {
+		return function (x, y, z) {
+			return make(
+				x + that(getX),
+				y + that(getY),
+				z + that(getZ)
+			);
+		}
+	}
+
+	function scale(s) {
+		return function (x, y, z) {
+			return make(
+				x * s,
+				y * s,
+				z * s
+			);
+		}
+	}
+
+	function length(x, y, z) {
+		return Math.sqrt(x * x + y * y + z * z);
+	}
+
+	// Wait, there's no need to copy vectors since they are immutable
+	function copy(x, y, z) {
+		// It would, actually, be just an alias for the constructor
+		return make(x, y, z);
+	}
+
+	// Export it all
+	return { make, getX, getY, getZ, add, scale, length };
+})();
+
+demo(implementation);
