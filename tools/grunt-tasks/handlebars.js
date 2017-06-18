50b6fb808fb9d336c0bd5c97cff4a89121200228
Adrian Toncean <adrianton3@gmail.com>
2017-06-18 15:32:12 +0200

    Modernise handlebars task

--- tools/grunt-tasks/handlebars.js
+++ tools/grunt-tasks/handlebars.js
@@ -1,22 +1,22 @@
-'use strict';
+'use strict'
 
-var fs = require('fs');
-var Handlebars = require('handlebars');
+const fs = require('fs')
+const Handlebars = require('handlebars')
 
 const { readFile } = require('../utils')
 
 module.exports = function (grunt) {
 	grunt.registerMultiTask('handlebars', 'Runs handlebars', function () {
-		var templateSrc = fs.readFileSync(this.data.templatePath, 'utf8');
+		const templateSrc = fs.readFileSync(this.data.templatePath, 'utf8')
 
-		var template = Handlebars.compile(templateSrc);
+		const template = Handlebars.compile(templateSrc)
 
 		const data = this.data.dataPath ?
 			JSON.parse(readFile(this.data.dataPath)) :
 			this.data.data
 
-		var result = template(data);
+		const result = template(data)
 
-		fs.writeFileSync(this.data.outPath, result);
+		fs.writeFileSync(this.data.outPath, result)
 	});
 };
